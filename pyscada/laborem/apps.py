e97c55a410396ebecd6ade0a5ba4a692a9722a33
Martin Schroeder <m.schroeder@tu-berlin.de>
2018-06-27 20:23:24 +0200

    fixed AppConfig

--- pyscada/laborem/apps.py
+++ pyscada/laborem/apps.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,3 +9,4 @@ from django.utils.translation import ugettext_lazy as _
 class PyScadaLaboREMConfig(AppConfig):
     name = 'pyscada.laborem'
     verbose_name = _("PyScada LaboREM")
+    path = os.path.realpath(__file__)
